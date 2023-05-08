3923b50bcb3d9b489962a1019724a29266da9c02
LasVegasBaby <taufiqnasrullah195@gmail.com>
2023-05-08 21:18:12 +0700

    Update Seeding

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,20 +1,81 @@
-import { PrismaClient } from '@prisma/client'
-import { parseISO } from 'date-fns';
-
+const { PrismaClient } = require('@prisma/client');
+// import { parseISO } from 'date-fns';
+const bcrypt = require("bcrypt");
 const prisma = new PrismaClient()
 
 async function main() {
-  const campaign1 = await prisma.campaign.create({
+const user1 = await prisma.users.create({
+    
     data: {
-      title: 'bantuan gempa',
-      description: 'menolong teman-teman yang kena gempa di aceh',
-      goal: 10000000,
-      currentDonation: 100000,
-      endDate: parseISO('2023-09-30 01:10:04.642'),
-      userId: 64,
+      name: 'Ali Doe',
+      email: 'AlieDoe@example.com',
+      password: await bcrypt.hash('test123', 10),
+      role: 'user'
     }
   })
-  console.log(campaign1)
+
+  const category1 = await prisma.categories.create({
+    data: {
+      name: 'Finance',
+      description: 'Funding Finance'
+    }
+  })
+
+  const campaign1 = await prisma.campaign.create({
+    data: {
+      title: 'Sawi Network',
+      description: 'New Network on Cryptocurrency',
+      goal: 1000000,
+      currentDonation: 0,
+      endDate: new Date('2023-08-30'),
+      user: {
+        connect: { id: user1.id }
+      },
+      categories: {
+        create: [{ category_id: category1.id }]
+      },
+      banner: {
+        create: [{ image: 'https://example.com/banner1.jpg' }]
+      }
+    }
+  })
+
+  const donation1 = await prisma.donations.create({
+    data: {
+      amount: 15000,
+      user: {
+        connect: { id: user1.id },
+      },
+      campaign: {
+        connect: { id: campaign1.id },
+      },
+    },
+  })
+
+  const comment1 = await prisma.comment.create({
+    data: {
+      content: "Mudah-mudahan JP",
+      user: {
+        connect: { id: user1.id },
+      },
+      campaign: {
+        connect: { id: campaign1.id },
+      },
+    },
+  })
+
+  const bookmark1 = await prisma.bookmark.create({
+    data: {
+      user: {
+        connect: { id: user1.id },
+      },
+      campaign: {
+        connect: { id: campaign1.id },
+      },
+    },
+  })
+
+  console.log('Seed successfully!');
 }
 
 main()
