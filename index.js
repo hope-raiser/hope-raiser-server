c60645a228fc91887c100cd52f0403a77e705bb9
zulfikar ilham <izulbustam@gmail.com>
2023-05-11 01:15:01 +0700

    add cors

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 3001;
 const router = require("./src/routes/index.js");
 const errorHandler = require("./src/middlewares/errorhandler.js");
+const cors = require('cors')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 // API DOCS
 const swaggerUi = require("swagger-ui-express");
